d341f9020165c7f7b4121b44e7135fda598ef147
Edwards <shad3e+github@pm.me>
2026-06-29 17:03:35 -0700

    First version of psuedocode

--- main.py
+++ main.py
@@ -5,3 +5,20 @@ import matplotlib.pyplot as plt
 
 # Data Set: https://archive.ics.uci.edu/dataset/518/speaker+accent+recognition
 
+def main():
+    # Step One: Predict accent from voice features and check fairness across accents
+    print("Hello, World")
+    # Step Two: Use Data Set
+
+    # Step Three: Explore different features (x) and samples per accent
+
+    # Step Four: Preprocess by cleaning data and splitting features and labels
+
+    # Step Five: Train classifer model on voice features
+
+    # Step Six: Evaluate overall accuracy then accuracy by accent group
+
+
+    # Step Seven: Improve data 
+    
+main()
