eebb17773426a86e11e28316401f3660d0fb5ea9
Edwards <shad3e+github@pm.me>
2026-07-01 12:11:19 -0700

    Fixed steps and found other data set

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
 # Potential libraries used
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression 
+from sklearn.tree import DecisionTreeClassifier
 
 
 # Data Set: https://archive.ics.uci.edu/dataset/518/speaker+accent+recognition
+# Other potential data set: https://accent.gmu.edu/
+
 
 def main():
-    # Step One: Predict accent from voice features and check fairness across accents
+    # Step One: Use Data Set
     print("Hello, World")
-    # Step Two: Use Data Set
-
-    # Step Three: Explore different features (x) and samples per accent
-
-    # Step Four: Preprocess by cleaning data and splitting features and labels
-
-    # Step Five: Train classifer model on voice features
-
-    # Step Six: Evaluate overall accuracy then accuracy by accent group
-
-
-    # Step Seven: Improve data 
     
-main()
+
+    # Step Two: Explore different features (x) and samples per accent
+
+    # Step Three: Preprocess by cleaning data and splitting features and labels
+
+    # Step Four: Train classifer model on voice features
+
+    # Step Five: Evaluate overall accuracy then accuracy by accent group
+
+
+    # Step Six: Improve data 
+    
+    # Step Seven: Predict the accent from voice features and check fairness across accents
+
+if __name__ == "__main__":
+    main()
